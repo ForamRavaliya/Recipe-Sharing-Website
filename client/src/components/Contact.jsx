17f06c35427e38568a85ccb43bde40201f88026e
ForamRavaliya <fravaliya2107@gmail.com>
2026-04-01 17:36:44 +0530

    update message

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -5,6 +5,7 @@ export default function Contact() {
     <div style={{ padding: "40px" }}>
       <h2>Contact Us</h2>
       <p>Email: support@recipeo.com</p>
+      <p>Mobile No. : 1145309210</p>
     </div>
   );
 }
